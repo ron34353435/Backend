c5af538159693f06af8bf0c8ff2871518a6cfab7
mikihalamish <61268887+mikihalamish@users.noreply.github.com>
2021-06-02 23:26:27 +0300

    Update test.js

--- test.js
+++ test.js
@@ -8,6 +8,12 @@ var client = null
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', (req, res) => {
     res.send("Empty (But working) Web Page")
